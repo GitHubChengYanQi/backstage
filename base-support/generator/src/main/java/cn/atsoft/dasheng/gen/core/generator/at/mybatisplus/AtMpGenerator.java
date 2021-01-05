2c188b714add8ef3dc6df4451c404062beb02490
Sing <78276478@qq.com>
2021-01-05 17:24:25 +0800

    完善代码生成器

--- base-support/generator/src/main/java/cn/atsoft/dasheng/gen/core/generator/at/mybatisplus/AtMpGenerator.java
+++ base-support/generator/src/main/java/cn/atsoft/dasheng/gen/core/generator/at/mybatisplus/AtMpGenerator.java
@@ -1,24 +1,16 @@
 package cn.atsoft.dasheng.gen.core.generator.at.mybatisplus;
 
-import cn.atsoft.dasheng.core.util.SpringContextHolder;
-import cn.atsoft.dasheng.core.util.ToolUtil;
-import cn.atsoft.dasheng.db.entity.DBFieldConfig;
-import cn.atsoft.dasheng.db.model.params.FieldConfigParam;
-import cn.atsoft.dasheng.db.service.FieldConfigService;
 import cn.atsoft.dasheng.gen.core.generator.base.AbstractMpGenerator;
 import cn.atsoft.dasheng.gen.core.enums.GenDownloadEnum;
 import cn.atsoft.dasheng.gen.core.generator.restful.mybatisplus.param.MpParam;
-import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.generator.config.FileOutConfig;
-import com.baomidou.mybatisplus.generator.config.po.TableField;
 import com.baomidou.mybatisplus.generator.config.po.TableFill;
 import com.baomidou.mybatisplus.generator.config.po.TableInfo;
 import com.baomidou.mybatisplus.generator.config.rules.DateType;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
-import org.beetl.core.Template;
 
 import java.io.File;
 import java.util.ArrayList;
