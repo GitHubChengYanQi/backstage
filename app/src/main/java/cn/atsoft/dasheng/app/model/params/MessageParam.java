5b73179c95fe9ed55a8c66f228308f879ad453f0
song <2625598959@qq.com>
2021-08-03 11:53:13 +0800

    添加消息提醒功能

---
+++ app/src/main/java/cn/atsoft/dasheng/app/model/params/MessageParam.java
@@ -0,0 +1,106 @@
+package cn.atsoft.dasheng.app.model.params;
+
+import lombok.Data;
+import cn.atsoft.dasheng.model.validator.BaseValidatingParam;
+import java.util.Date;
+import java.io.Serializable;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import java.util.List;
+
+/**
+ * <p>
+ * 消息提醒
+ * </p>
+ *
+ * @author 
+ * @since 2021-08-03
+ */
+@Data
+@ApiModel
+public class MessageParam implements Serializable, BaseValidatingParam {
+
+    private static final long serialVersionUID = 1L;
+
+
+    /**
+     * 消息提醒id
+     */
+    @ApiModelProperty("消息提醒id")
+    private Long messageId;
+
+    /**
+     * 提醒时间
+     */
+    @ApiModelProperty("提醒时间")
+    private Date time;
+
+    /**
+     * 提醒状态
+     */
+    @ApiModelProperty("提醒状态")
+    private Integer state;
+
+    /**
+     * 提醒标题
+     */
+    @ApiModelProperty("提醒标题")
+    private String title;
+
+    /**
+     * 提醒内容
+     */
+    @ApiModelProperty("提醒内容")
+    private String content;
+
+    /**
+     * 提醒类型
+     */
+    @ApiModelProperty("提醒类型")
+    private Integer type;
+
+    /**
+     * 创建者
+     */
+    @ApiModelProperty(hidden = true)
+    private Long createUser;
+
+    /**
+     * 修改者
+     */
+    @ApiModelProperty(hidden = true)
+    private Long updateUser;
+
+    /**
+     * 创建时间
+     */
+    @ApiModelProperty(hidden = true)
+    private Date createTime;
+
+    /**
+     * 修改时间
+     */
+    @ApiModelProperty(hidden = true)
+    private Date updateTime;
+
+    /**
+     * 状态
+     */
+    @ApiModelProperty("状态")
+    private Integer display;
+
+    /**
+     * 排序
+     */
+    @ApiModelProperty("排序")
+    private Long sort;
+
+    @ApiModelProperty("父ID顺序数组")
+    private List<String> pidValue;
+
+    @Override
+    public String checkParam() {
+        return null;
+    }
+
+}
---
+++ app/src/main/java/cn/atsoft/dasheng/app/service/impl/MessageServiceImpl.java
@@ -0,0 +1,85 @@
+package cn.atsoft.dasheng.app.service.impl;
+
+
+import cn.atsoft.dasheng.base.pojo.page.PageFactory;
+import cn.atsoft.dasheng.base.pojo.page.PageInfo;
+import cn.atsoft.dasheng.app.entity.Message;
+import cn.atsoft.dasheng.app.mapper.MessageMapper;
+import cn.atsoft.dasheng.app.model.params.MessageParam;
+import cn.atsoft.dasheng.app.model.result.MessageResult;
+import  cn.atsoft.dasheng.app.service.MessageService;
+import cn.atsoft.dasheng.core.util.ToolUtil;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * <p>
+ * 消息提醒 服务实现类
+ * </p>
+ *
+ * @author 
+ * @since 2021-08-03
+ */
+@Service
+public class MessageServiceImpl extends ServiceImpl<MessageMapper, Message> implements MessageService {
+
+    @Override
+    public void add(MessageParam param){
+        Message entity = getEntity(param);
+        this.save(entity);
+    }
+
+    @Override
+    public void delete(MessageParam param){
+        this.removeById(getKey(param));
+    }
+
+    @Override
+    public void update(MessageParam param){
+        Message oldEntity = getOldEntity(param);
+        Message newEntity = getEntity(param);
+        ToolUtil.copyProperties(newEntity, oldEntity);
+        this.updateById(newEntity);
+    }
+
+    @Override
+    public MessageResult findBySpec(MessageParam param){
+        return null;
+    }
+
+    @Override
+    public List<MessageResult> findListBySpec(MessageParam param){
+        return null;
+    }
+
+    @Override
+    public PageInfo<MessageResult> findPageBySpec(MessageParam param){
+        Page<MessageResult> pageContext = getPageContext();
+        IPage<MessageResult> page = this.baseMapper.customPageList(pageContext, param);
+        return PageFactory.createPageInfo(page);
+    }
+
+    private Serializable getKey(MessageParam param){
+        return param.getMessageId();
+    }
+
+    private Page<MessageResult> getPageContext() {
+        return PageFactory.defaultPage();
+    }
+
+    private Message getOldEntity(MessageParam param) {
+        return this.getById(getKey(param));
+    }
+
+    private Message getEntity(MessageParam param) {
+        Message entity = new Message();
+        ToolUtil.copyProperties(param, entity);
+        return entity;
+    }
+
+}
