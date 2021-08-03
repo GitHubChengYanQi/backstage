30ad7fac59a3ddf90e3c140e730af77ab068b53c
song <2625598959@qq.com>
2021-08-03 10:02:17 +0800

    修改商机属性

--- app/src/main/java/cn/atsoft/dasheng/app/entity/Business.java
+++ app/src/main/java/cn/atsoft/dasheng/app/entity/Business.java
@@ -132,114 +132,9 @@ public class Business implements Serializable {
   @TableField("order_discount")
   private String orderDiscount ;
 
-
-  /**
-   *输单原因
-   */
-  @TableField("reason")
-  private String reason ;
-
-  /**
-   *主线索
-   */
-  @TableField("main_cable")
-  private String mainCable ;
-
-
-    /**
-     * 创建时间
-     */
-    @TableField(value = "create_time", fill = FieldFill.INSERT)
-    private Date createTime;
-
-    /**
-     * 修改时间
-     */
-    @TableField(value = "update_time", fill = FieldFill.UPDATE)
-    private Date updateTime;
-
-    /**
-     * 创建用户
-     */
-    @TableField(value = "create_user", fill = FieldFill.INSERT)
-    private Long createUser;
-
-    /**
-     * 修改用户
-     */
-    @TableField(value = "update_user", fill = FieldFill.UPDATE)
-    private Long updateUser;
-
-    /**
-     * 状态
-     */
-    @TableField("display")
-    private Integer display;
-  /**
-   * 部门编号
-   */
-  @TableField("deptId")
-  private Long deptId;
-
-  /**
-   * 商机名称
-   */
-  @TableField("business_name")
-  private String businessName;
-
-  /**
-   * 结单日期
-   */
-  @TableField("statement_time")
-  private Date statementTime;
-
-  /**
-   * 销售流程id
-   */
-  @TableField("sales_id")
-  private Long salesId;
-
-  /**
-   * 赢率id
-   */
-  @TableField("sales_process_id")
-  private Long salesProcessId;
-
-  /**
-   * 阶段变更时间
-   */
-  @TableField("change_time")
-  private Date changeTime;
-
-  /**
-   * 商机金额
-   */
-  @TableField("opportunity_amount")
-  private int opportunityAmount;
-
-  /**
-   * 产品合计
-   */
-  @TableField("total_products")
-  private Double totalProducts;
-
-  /**
-   * 整单折扣
-   */
-  @TableField("order_discount")
-  private String orderDiscount;
-
-  /**
-   * 输单原因
-   */
-  @TableField("reason")
-  private String reason;
-
-  /**
-   * 主线索
-   */
-  @TableField("main_cable")
-  private String mainCable;
+  public static long getSerialVersionUID() {
+    return serialVersionUID;
+  }
 
   public Long getBusinessId() {
     return businessId;
@@ -303,143 +198,6 @@ public class Business implements Serializable {
 
   public void setPerson(Long person) {
     this.person = person;
-  }
-
-  public Date getCreateTime() {
-    return createTime;
-  }
-
-  public void setCreateTime(Date createTime) {
-    this.createTime = createTime;
-  }
-
-  public Date getUpdateTime() {
-    return updateTime;
-  }
-
-  public void setUpdateTime(Date updateTime) {
-    this.updateTime = updateTime;
-  }
-
-  public Long getCreateUser() {
-    return createUser;
-  }
-
-  public void setCreateUser(Long createUser) {
-    this.createUser = createUser;
-  }
-
-
-  public Long getBusinessId() {
-    return businessId;
-  }
-
-  public void setBusinessId(Long businessId) {
-    this.businessId = businessId;
-  }
-
-  public Long getCustomerId() {
-    return customerId;
-  }
-
-  public void setCustomerId(Long customerId) {
-    this.customerId = customerId;
-  }
-
-  public Long getStockId() {
-    return stockId;
-  }
-
-  public void setStockId(Long stockId) {
-    this.stockId = stockId;
-  }
-
-  public long getOriginId() {
-    return originId;
-  }
-
-  public void setOriginId(long originId) {
-    this.originId = originId;
-  }
-
-  public String getTime() {
-    return time;
-  }
-
-  public void setTime(String time) {
-    this.time = time;
-  }
-
-  public String getState() {
-    return state;
-  }
-
-  public void setState(String state) {
-    this.state = state;
-  }
-
-  public String getStage() {
-    return stage;
-  }
-
-  public void setStage(String stage) {
-    this.stage = stage;
-  }
-
-  public Long getPerson() {
-    return person;
-  }
-
-  public void setPerson(Long person) {
-    this.person = person;
-  }
-
-  public Long getDeptId() {
-    return deptId;
-  }
-
-  public void setDeptId(Long deptId) {
-    this.deptId = deptId;
-  }
-
-  public String getBusinessName() {
-    return businessName;
-  }
-
-  public void setBusinessName(String businessName) {
-    this.businessName = businessName;
-  }
-
-  public Date getStatementTime() {
-    return statementTime;
-  }
-
-  public void setStatementTime(Date statementTime) {
-    this.statementTime = statementTime;
-  }
-
-  public Long getSalesId() {
-    return salesId;
-  }
-
-  public void setSalesId(Long salesId) {
-    this.salesId = salesId;
-  }
-
-  public Long getUpdateUser() {
-    return updateUser;
-  }
-
-  public void setUpdateUser(Long updateUser) {
-    this.updateUser = updateUser;
-  }
-
-  public Integer getDisplay() {
-    return display;
-  }
-
-  public void setDisplay(Integer display) {
-    this.display = display;
   }
 
   public Long getDeptId() {
@@ -569,6 +327,57 @@ public class Business implements Serializable {
   public void setDisplay(Integer display) {
     this.display = display;
   }
+
+  /**
+   *输单原因
+   */
+  @TableField("reason")
+  private String reason ;
+
+  /**
+   *主线索
+   */
+  @TableField("main_cable")
+  private String mainCable ;
+
+
+    /**
+     * 创建时间
+     */
+    @TableField(value = "create_time", fill = FieldFill.INSERT)
+    private Date createTime;
+
+    /**
+     * 修改时间
+     */
+    @TableField(value = "update_time", fill = FieldFill.UPDATE)
+    private Date updateTime;
+
+    /**
+     * 创建用户
+     */
+    @TableField(value = "create_user", fill = FieldFill.INSERT)
+    private Long createUser;
+
+    /**
+     * 修改用户
+     */
+    @TableField(value = "update_user", fill = FieldFill.UPDATE)
+    private Long updateUser;
+
+    /**
+     * 状态
+     */
+    @TableField("display")
+    private Integer display;
+  /**
+   * 部门编号
+   */
+
+  /**
+   * 主线索
+   */
+
 
   @Override
   public String toString() {
--- app/src/main/java/cn/atsoft/dasheng/app/model/params/BusinessParam.java
+++ app/src/main/java/cn/atsoft/dasheng/app/model/params/BusinessParam.java
@@ -99,71 +99,8 @@ private  Long originId;
   /**
    * 部门编号
    */
-  @ApiModelProperty("部门编号")
-  private Long deptId;
-
-  /**
-   * 商机名称
-   */
-  @ApiModelProperty("商机名称")
-  private String businessName;
-
-  /**
-   * 结单日期
-   */
-  @ApiModelProperty("结单日期")
-  private Date statementTime;
-
-  /**
-   * 销售流程id
-   */
-  @ApiModelProperty("销售流程id")
-  private Long salesId;
 
 
-  /**
-   * 赢率id
-   */
-  @ApiModelProperty("赢率id")
-  private Long salesProcessId;
-
-  /**
-   * 阶段变更时间
-   */
-  @ApiModelProperty("阶段变更时间")
-  private Date changeTime;
-
-
-  /**
-   * 商机金额
-   */
-  @ApiModelProperty("商机金额")
-  private int opportunityAmount;
-
-
-  /**
-   * 产品合计
-   */
-  @ApiModelProperty("产品合计")
-  private Double totalProducts;
-
-  /**
-   * 整单折扣
-   */
-  @ApiModelProperty("整单折扣")
-  private String orderDiscount;
-
-  /**
-   * 输单原因
-   */
-  @ApiModelProperty("整单折扣")
-  private String reason;
-
-  /**
-   * 主线索
-   */
-  @ApiModelProperty("主线索")
-  private String mainCable;
     private  String name;
     /**
      * 创建时间
