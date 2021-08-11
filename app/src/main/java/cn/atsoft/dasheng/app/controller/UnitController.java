823f057120e4114347a3f1a21a7e742e07dd840a
siqiang <565902317@qq.com>
2021-08-11 15:42:41 +0800

    新增单位和产品分类表数据处理

---
+++ app/src/main/java/cn/atsoft/dasheng/app/controller/UnitController.java
@@ -0,0 +1,112 @@
+package cn.atsoft.dasheng.app.controller;
+
+import cn.atsoft.dasheng.base.pojo.page.PageInfo;
+import cn.atsoft.dasheng.app.entity.Unit;
+import cn.atsoft.dasheng.app.model.params.UnitParam;
+import cn.atsoft.dasheng.app.model.result.UnitResult;
+import cn.atsoft.dasheng.app.service.UnitService;
+import cn.atsoft.dasheng.core.base.controller.BaseController;
+import cn.atsoft.dasheng.core.util.ToolUtil;
+import cn.atsoft.dasheng.model.response.ResponseData;
+import cn.hutool.core.convert.Convert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+
+/**
+ * 单位表控制器
+ *
+ * @author cheng
+ * @Date 2021-08-11 15:37:57
+ */
+@RestController
+@RequestMapping("/unit")
+@Api(tags = "单位表")
+public class UnitController extends BaseController {
+
+    @Autowired
+    private UnitService unitService;
+
+    /**
+     * 新增接口
+     *
+     * @author cheng
+     * @Date 2021-08-11
+     */
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    @ApiOperation("新增")
+    public ResponseData addItem(@RequestBody UnitParam unitParam) {
+        this.unitService.add(unitParam);
+        return ResponseData.success();
+    }
+
+    /**
+     * 编辑接口
+     *
+     * @author cheng
+     * @Date 2021-08-11
+     */
+    @RequestMapping(value = "/edit", method = RequestMethod.POST)
+    @ApiOperation("编辑")
+    public ResponseData update(@RequestBody UnitParam unitParam) {
+
+        this.unitService.update(unitParam);
+        return ResponseData.success();
+    }
+
+    /**
+     * 删除接口
+     *
+     * @author cheng
+     * @Date 2021-08-11
+     */
+    @RequestMapping(value = "/delete", method = RequestMethod.POST)
+    @ApiOperation("删除")
+    public ResponseData delete(@RequestBody UnitParam unitParam)  {
+        this.unitService.delete(unitParam);
+        return ResponseData.success();
+    }
+
+    /**
+     * 查看详情接口
+     *
+     * @author cheng
+     * @Date 2021-08-11
+     */
+    @RequestMapping(value = "/detail", method = RequestMethod.POST)
+    @ApiOperation("详情")
+    public ResponseData<UnitResult> detail(@RequestBody UnitParam unitParam) {
+        Unit detail = this.unitService.getById(unitParam.getUnitId());
+        UnitResult result = new UnitResult();
+        ToolUtil.copyProperties(detail, result);
+
+        result.setValue(parentValue);
+        return ResponseData.success(result);
+    }
+
+    /**
+     * 查询列表
+     *
+     * @author cheng
+     * @Date 2021-08-11
+     */
+    @RequestMapping(value = "/list", method = RequestMethod.POST)
+    @ApiOperation("列表")
+    public PageInfo<UnitResult> list(@RequestBody(required = false) UnitParam unitParam) {
+        if(ToolUtil.isEmpty(unitParam)){
+            unitParam = new UnitParam();
+        }
+        return this.unitService.findPageBySpec(unitParam);
+    }
+
+
+
+
+}
+
+
---
+++ app/src/main/java/cn/atsoft/dasheng/app/model/params/UnitParam.java
@@ -0,0 +1,82 @@
+package cn.atsoft.dasheng.app.model.params;
+
+import lombok.Data;
+import cn.atsoft.dasheng.model.validator.BaseValidatingParam;
+import java.util.Date;
+import java.io.Serializable;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import java.util.List;
+
+/**
+ * <p>
+ * 单位表
+ * </p>
+ *
+ * @author cheng
+ * @since 2021-08-11
+ */
+@Data
+@ApiModel
+public class UnitParam implements Serializable, BaseValidatingParam {
+
+    private static final long serialVersionUID = 1L;
+
+
+    /**
+     * 单位id
+     */
+    @ApiModelProperty("单位id")
+    private Long unitId;
+
+    /**
+     * 单位名称
+     */
+    @ApiModelProperty("单位名称")
+    private String unitName;
+
+    /**
+     * 创建者
+     */
+    @ApiModelProperty(hidden = true)
+    private Long createUser;
+
+    /**
+     * 创建时间
+     */
+    @ApiModelProperty(hidden = true)
+    private Date createTime;
+
+    /**
+     * 修改者
+     */
+    @ApiModelProperty(hidden = true)
+    private Long updateUser;
+
+    /**
+     * 修改时间
+     */
+    @ApiModelProperty(hidden = true)
+    private Date updateTime;
+
+    /**
+     * 状态
+     */
+    @ApiModelProperty("状态")
+    private Integer display;
+
+    /**
+     * 部门编号
+     */
+    @ApiModelProperty("部门编号")
+    private Long deptId;
+
+    @ApiModelProperty("父ID顺序数组")
+    private List<String> pidValue;
+
+    @Override
+    public String checkParam() {
+        return null;
+    }
+
+}
