7670ad3d9a31c475d9ff0a26295f8e6b20517ee4
siqiang <565902317@qq.com>
2021-08-16 13:45:21 +0800

    修改套餐管理接口

--- app/src/main/java/cn/atsoft/dasheng/app/service/impl/ErpPackageTableServiceImpl.java
+++ app/src/main/java/cn/atsoft/dasheng/app/service/impl/ErpPackageTableServiceImpl.java
@@ -5,6 +5,7 @@ import cn.atsoft.dasheng.app.entity.Brand;
 import cn.atsoft.dasheng.app.entity.Items;
 import cn.atsoft.dasheng.app.model.result.BrandResult;
 import cn.atsoft.dasheng.app.model.result.ItemsResult;
+import cn.atsoft.dasheng.app.model.result.StockResult;
 import cn.atsoft.dasheng.app.service.BrandService;
 import cn.atsoft.dasheng.app.service.ItemsService;
 import cn.atsoft.dasheng.base.pojo.page.PageFactory;
@@ -74,38 +75,9 @@ public class ErpPackageTableServiceImpl extends ServiceImpl<ErpPackageTableMappe
     public PageInfo<ErpPackageTableResult> findPageBySpec(ErpPackageTableParam param){
         Page<ErpPackageTableResult> pageContext = getPageContext();
         IPage<ErpPackageTableResult> page = this.baseMapper.customPageList(pageContext, param);
-        List<Long> wp=new ArrayList<>();
-        List<Long> wp1=new ArrayList<>();
-        if (ToolUtil.isNotEmpty(page.getRecords())){
-            for (ErpPackageTableResult record : page.getRecords()) {
-                wp.add(record.getItemId());
-                wp1.add(record.getBrandId());
-            }
-            QueryWrapper<Items> queryWrapper = new QueryWrapper<>();
-            QueryWrapper<Brand> queryWrapper1=new QueryWrapper<>();
-            queryWrapper.in("item_id",wp);
-            queryWrapper1.in("brand_id",wp1);
-            List<Items> list=itemsService.list(queryWrapper);
-            List<Brand> list1=brandService.list(queryWrapper1);
-            for (ErpPackageTableResult record : page.getRecords()) {
-                for (Items items : list) {
-                    if(items.getItemId().equals(record.getItemId())){
-                        ItemsResult itemsResult =new ItemsResult();
-                        ToolUtil.copyProperties(items,itemsResult);
-                        record.setItemsResult(itemsResult);
-                        break;
-                    }
-                }
-                for (Brand brand : list1) {
-                    BrandResult brandResult=new BrandResult();
-                    ToolUtil.copyProperties(brand,brandResult);
-                    record.setBrandResult(brandResult);
-                    break;
-                }
-            }
-        }
-
+        format(page.getRecords());
         return PageFactory.createPageInfo(page);
+
     }
 
     private Serializable getKey(ErpPackageTableParam param){
@@ -126,4 +98,44 @@ public class ErpPackageTableServiceImpl extends ServiceImpl<ErpPackageTableMappe
         return entity;
     }
 
+    public void format (List<ErpPackageTableResult> data){
+        List<Long> itemIds=new ArrayList<>();
+        List<Long> brandeIds=new ArrayList<>();
+        if (ToolUtil.isNotEmpty(data)){
+            for (ErpPackageTableResult record : data) {
+                itemIds.add(record.getItemId());
+                brandeIds.add(record.getBrandId());
+            }
+
+            QueryWrapper<Items> ItemsQueryWrapper = new QueryWrapper<>();
+            if(ToolUtil.isNotEmpty(itemIds)){
+                ItemsQueryWrapper.in("item_id",itemIds);
+            }
+            List<Items> ItemsList=itemsService.list(ItemsQueryWrapper);
+
+            QueryWrapper<Brand> BrandQueryWrapper=new QueryWrapper<>();
+            if(ToolUtil.isNotEmpty(brandeIds)){
+                BrandQueryWrapper.in("brand_id",brandeIds);
+            }
+            List<Brand> BrandList=brandService.list(BrandQueryWrapper);
+            for (ErpPackageTableResult record : data) {
+                for (Items items : ItemsList) {
+                    if(items.getItemId().equals(record.getItemId())){
+                        ItemsResult itemsResult =new ItemsResult();
+                        ToolUtil.copyProperties(items,itemsResult);
+                        record.setItemsResult(itemsResult);
+                        break;
+                    }
+                }
+                for (Brand brand : BrandList) {
+                    if(brand.getBrandId().equals(record.getBrandId())) {
+                        BrandResult brandResult = new BrandResult();
+                        ToolUtil.copyProperties(brand, brandResult);
+                        record.setBrandResult(brandResult);
+                        break;
+                    }
+                }
+            }
+        }
+    }
 }
