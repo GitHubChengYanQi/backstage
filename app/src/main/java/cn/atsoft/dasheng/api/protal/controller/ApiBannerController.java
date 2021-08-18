72cece15aae650d7e08e84e21c53de1813fc8482
song <2625598959@qq.com>
2021-08-18 11:11:29 +0800

    添加前端轮播图和导航接口

---
+++ app/src/main/java/cn/atsoft/dasheng/api/protal/controller/ApiBannerController.java
@@ -0,0 +1,29 @@
+package cn.atsoft.dasheng.api.protal.controller;
+
+import cn.atsoft.dasheng.portal.banner.entity.Banner;
+import cn.atsoft.dasheng.portal.banner.model.response.ResponseData;
+import cn.atsoft.dasheng.portal.banner.service.BannerService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class ApiBannerController {
+
+    @Autowired
+    private BannerService bannerService;
+
+    @RequestMapping(value = "/getBanner", method = RequestMethod.GET)
+    public ResponseData getBanner(@RequestParam("classId") Long classId) {
+        QueryWrapper<Banner> bannerQueryWrapper = new QueryWrapper<>();
+        bannerQueryWrapper.in("difference", classId);
+        List<Banner> list = bannerService.list(bannerQueryWrapper);
+        return ResponseData.success(list);
+    }
+}
