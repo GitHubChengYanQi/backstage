c95380e2f1c72df98245c6585b97653ed5cf03c8
song <2625598959@qq.com>
2021-08-19 08:41:08 +0800

    添加二级分类下拉功能

---
+++ app/src/main/java/cn/atsoft/dasheng/api/shop/controller/ClassController.java
@@ -0,0 +1,13 @@
+package cn.atsoft.dasheng.api.shop.controller;
+
+import cn.atsoft.dasheng.portal.banner.model.response.ResponseData;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ClassController {
+    public ResponseData getclass (){
+        return  null;
+    }
+}
