01919659398291420ff217dd6d16e2e51af050ef
song <2625598959@qq.com>
2021-08-19 13:12:07 +0800

    修复二级分类引包问题

--- app/src/main/java/cn/atsoft/dasheng/api/shop/controller/ClassdifController.java
+++ app/src/main/java/cn/atsoft/dasheng/api/shop/controller/ClassdifController.java
@@ -6,6 +6,7 @@ import cn.atsoft.dasheng.shop.classdifference.service.ClassDifferenceService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import cn.atsoft.dasheng.shop.classpage.model.params.ClassParam;
 
 
 import java.util.List;
@@ -17,9 +18,9 @@ public class ClassdifController {
     private ClassDifferenceService service;
 
     @RequestMapping(value = "/getclassdifference", method = RequestMethod.POST)
-    public ResponseData getclassdifference(@RequestBody cn.atsoft.dasheng.shop.classpage.model.params.ClassParam classParam) {
+    public ResponseData getclassdifference(@RequestBody ClassParam classParam) {
         QueryWrapper<ClassDifference> queryWrapper = new QueryWrapper<>();
-        queryWrapper.in("class_id", classParam.getClassId()).orderByAsc("sort").in("display",1);
+        queryWrapper.in("class_id", classParam.getClassId()).orderByAsc("sort").in("display", 1);
         List<ClassDifference> list = service.list(queryWrapper);
         return ResponseData.success(list);
     }
