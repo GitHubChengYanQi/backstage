ca8686223c97f18878d27d348a6b45644448e86a
song <2625598959@qq.com>
2021-08-20 17:28:57 +0800

    发货详情映射客户

--- app/src/main/java/cn/atsoft/dasheng/portal/companyaddress/controller/CompanyAddressController.java
+++ app/src/main/java/cn/atsoft/dasheng/portal/companyaddress/controller/CompanyAddressController.java
@@ -85,7 +85,7 @@ public class CompanyAddressController extends BaseController {
         CompanyAddressResult result = new CompanyAddressResult();
         ToolUtil.copyProperties(detail, result);
 
-        result.setValue(parentValue);
+
         return ResponseData.success(result);
     }
 
