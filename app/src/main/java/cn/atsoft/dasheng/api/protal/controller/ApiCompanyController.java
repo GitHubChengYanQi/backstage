f55e402c76d5c3fad5d239135af3aa9f5e68a031
siqiang <565902317@qq.com>
2021-08-23 14:36:45 +0800

    设备选择

--- app/src/main/java/cn/atsoft/dasheng/api/protal/controller/ApiCompanyController.java
+++ app/src/main/java/cn/atsoft/dasheng/api/protal/controller/ApiCompanyController.java
@@ -1,13 +1,21 @@
 package cn.atsoft.dasheng.api.protal.controller;
 
+import cn.atsoft.dasheng.app.entity.Customer;
+import cn.atsoft.dasheng.app.service.CustomerService;
+import cn.atsoft.dasheng.app.wrapper.CustomerSelectWrapper;
 import cn.atsoft.dasheng.model.response.ResponseData;
 import cn.atsoft.dasheng.portal.companyaddress.model.params.CompanyAddressParam;
 import cn.atsoft.dasheng.portal.companyaddress.service.CompanyAddressService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -15,10 +23,18 @@ public class ApiCompanyController {
 
     @Autowired
     private CompanyAddressService companyAddressService;
+    @Autowired
+    private CustomerService customerService;
 
-    @RequestMapping(value = "/saveCompanny", method = RequestMethod.POST)
-    public ResponseData saveCompanny(@RequestBody CompanyAddressParam companyAddressParam) {
-        this.companyAddressService.add(companyAddressParam);
-        return ResponseData.success();
+    @RequestMapping(value = "/getCompannyList", method = RequestMethod.GET)
+    @ApiOperation("Select数据接口")
+    public ResponseData<List<Map<String, Object>>> listSelect() {
+        QueryWrapper<Customer> queryWrapper = new QueryWrapper();
+        queryWrapper.in("display",1);
+        List<Map<String, Object>> list = this.customerService.listMaps(queryWrapper);
+        CustomerSelectWrapper customerSelectWrapper = new CustomerSelectWrapper(list);
+        List<Map<String, Object>> result = customerSelectWrapper.wrap();
+        return ResponseData.success(result);
     }
+
 }
