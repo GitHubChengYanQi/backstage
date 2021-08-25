77af758d18e22fa7c9bd979c2bfff6ef1c5bc65e
song <2625598959@qq.com>
2021-08-25 19:28:43 +0800

    关联表映射user

--- app/src/main/java/cn/atsoft/dasheng/portal/wxUser/service/impl/WxuserInfoServiceImpl.java
+++ app/src/main/java/cn/atsoft/dasheng/portal/wxUser/service/impl/WxuserInfoServiceImpl.java
@@ -7,14 +7,20 @@ import cn.atsoft.dasheng.portal.wxUser.entity.WxuserInfo;
 import cn.atsoft.dasheng.portal.wxUser.mapper.WxuserInfoMapper;
 import cn.atsoft.dasheng.portal.wxUser.model.params.WxuserInfoParam;
 import cn.atsoft.dasheng.portal.wxUser.model.result.WxuserInfoResult;
-import  cn.atsoft.dasheng.portal.wxUser.service.WxuserInfoService;
+import cn.atsoft.dasheng.portal.wxUser.service.WxuserInfoService;
 import cn.atsoft.dasheng.core.util.ToolUtil;
+import cn.atsoft.dasheng.sys.modular.system.entity.User;
+import cn.atsoft.dasheng.sys.modular.system.model.result.UserResult;
+import cn.atsoft.dasheng.sys.modular.system.service.UserService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -22,25 +28,27 @@ import java.util.List;
  * 用户 小程序 关联 服务实现类
  * </p>
  *
- * @author 
+ * @author
  * @since 2021-08-24
  */
 @Service
 public class WxuserInfoServiceImpl extends ServiceImpl<WxuserInfoMapper, WxuserInfo> implements WxuserInfoService {
+    @Autowired
+    private UserService userService;
 
     @Override
-    public void add(WxuserInfoParam param){
+    public void add(WxuserInfoParam param) {
         WxuserInfo entity = getEntity(param);
         this.save(entity);
     }
 
     @Override
-    public void delete(WxuserInfoParam param){
+    public void delete(WxuserInfoParam param) {
         this.removeById(getKey(param));
     }
 
     @Override
-    public void update(WxuserInfoParam param){
+    public void update(WxuserInfoParam param) {
         WxuserInfo oldEntity = getOldEntity(param);
         WxuserInfo newEntity = getEntity(param);
         ToolUtil.copyProperties(newEntity, oldEntity);
@@ -48,23 +56,44 @@ public class WxuserInfoServiceImpl extends ServiceImpl<WxuserInfoMapper, WxuserI
     }
 
     @Override
-    public WxuserInfoResult findBySpec(WxuserInfoParam param){
+    public WxuserInfoResult findBySpec(WxuserInfoParam param) {
         return null;
     }
 
     @Override
-    public List<WxuserInfoResult> findListBySpec(WxuserInfoParam param){
+    public List<WxuserInfoResult> findListBySpec(WxuserInfoParam param) {
         return null;
     }
 
     @Override
-    public PageInfo<WxuserInfoResult> findPageBySpec(WxuserInfoParam param){
+    public PageInfo<WxuserInfoResult> findPageBySpec(WxuserInfoParam param) {
         Page<WxuserInfoResult> pageContext = getPageContext();
         IPage<WxuserInfoResult> page = this.baseMapper.customPageList(pageContext, param);
+
+        List<Long> ids = new ArrayList<>();
+        for (WxuserInfoResult record : page.getRecords()) {
+            ids.add(record.getMemberId());
+        }
+        QueryWrapper<User> userQueryWrapper = new QueryWrapper<>();
+        userQueryWrapper.in("user_id", ids);
+
+        List<User> users = userService.list(userQueryWrapper);
+
+        for (WxuserInfoResult record : page.getRecords()) {
+            for (User user : users) {
+                if (user.getUserId().equals(record.getUserId())) {
+                    UserResult userResult = new UserResult();
+                    ToolUtil.copyProperties(user, userResult);
+                    record.setUserResult(userResult);
+                    break;
+                }
+
+            }
+        }
         return PageFactory.createPageInfo(page);
     }
 
-    private Serializable getKey(WxuserInfoParam param){
+    private Serializable getKey(WxuserInfoParam param) {
         return param.getUserInfoId();
     }
 
