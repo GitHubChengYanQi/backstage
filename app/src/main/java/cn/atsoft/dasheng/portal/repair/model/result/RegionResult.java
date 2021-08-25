c9ecdf8db790c7aefd2f6fe3a79cde8cff620cf8
song <2625598959@qq.com>
2021-08-25 16:56:37 +0800

    aaa

---
+++ app/src/main/java/cn/atsoft/dasheng/portal/repair/model/result/RegionResult.java
@@ -0,0 +1,12 @@
+package cn.atsoft.dasheng.portal.repair.model.result;
+
+import lombok.Data;
+
+@Data
+public class RegionResult {
+    private  String countries;
+    private  String province;
+    private  String city;
+    private  String area;
+
+}
