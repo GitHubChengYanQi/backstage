c587ecd73d523a870a164be6928c3c8c2bb34cbf
siqiang <565902317@qq.com>
2021-08-30 16:42:35 +0800

    修改

--- app/src/main/java/cn/atsoft/dasheng/portal/remind/service/impl/RemindServiceImpl.java
+++ app/src/main/java/cn/atsoft/dasheng/portal/remind/service/impl/RemindServiceImpl.java
@@ -48,7 +48,7 @@ public class RemindServiceImpl extends ServiceImpl<RemindMapper, Remind> impleme
     @Override
     public void add(RemindParam param) {
 
-         wxTemplate.template(param.getUsers(),param.getType());
+        wxTemplate.template(param.getType());
 
         Long type = param.getType();
         QueryWrapper<Remind> queryWrapper = new QueryWrapper<>();
