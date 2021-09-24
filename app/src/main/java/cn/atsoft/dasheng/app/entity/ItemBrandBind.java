a084bce5247c9660563540fb5d75f10e0283552d
captain_jazz <captain_jazz@foxmail.com>
2021-09-24 09:34:51 +0800

    商品品牌绑定表

---
+++ app/src/main/java/cn/atsoft/dasheng/app/entity/ItemBrandBind.java
@@ -0,0 +1,150 @@
+package cn.atsoft.dasheng.app.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.IdType;
+import java.util.Date;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 商品品牌绑定表
+ * </p>
+ *
+ * @author 
+ * @since 2021-09-24
+ */
+@TableName("daoxin_erp_item_brand_bind")
+public class ItemBrandBind implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 产品品牌绑定表
+     */
+      @TableId(value = "item_brand_bind_id", type = IdType.ID_WORKER)
+    private Long itemBrandBindId;
+
+    /**
+     * 商品id
+     */
+    @TableField("item_id")
+    private Long itemId;
+
+    /**
+     * 品牌id
+     */
+    @TableField("brand_id")
+    private Long brandId;
+
+      @TableField(value = "create_time", fill = FieldFill.INSERT)
+    private Date createTime;
+
+      @TableField(value = "create_user", fill = FieldFill.INSERT)
+    private Long createUser;
+
+      @TableField(value = "update_time", fill = FieldFill.UPDATE)
+    private Date updateTime;
+
+      @TableField(value = "update_user", fill = FieldFill.UPDATE)
+    private Long updateUser;
+
+    @TableField("display")
+    private Integer display;
+
+    /**
+     * 部门编号
+     */
+    @TableField("deptId")
+    private Long deptId;
+
+
+    public Long getItemBrandBindId() {
+        return itemBrandBindId;
+    }
+
+    public void setItemBrandBindId(Long itemBrandBindId) {
+        this.itemBrandBindId = itemBrandBindId;
+    }
+
+    public Long getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Long itemId) {
+        this.itemId = itemId;
+    }
+
+    public Long getBrandId() {
+        return brandId;
+    }
+
+    public void setBrandId(Long brandId) {
+        this.brandId = brandId;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Long getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(Long createUser) {
+        this.createUser = createUser;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Long getUpdateUser() {
+        return updateUser;
+    }
+
+    public void setUpdateUser(Long updateUser) {
+        this.updateUser = updateUser;
+    }
+
+    public Integer getDisplay() {
+        return display;
+    }
+
+    public void setDisplay(Integer display) {
+        this.display = display;
+    }
+
+    public Long getDeptId() {
+        return deptId;
+    }
+
+    public void setDeptId(Long deptId) {
+        this.deptId = deptId;
+    }
+
+    @Override
+    public String toString() {
+        return "ItemBrandBind{" +
+        "itemBrandBindId=" + itemBrandBindId +
+        ", itemId=" + itemId +
+        ", brandId=" + brandId +
+        ", createTime=" + createTime +
+        ", createUser=" + createUser +
+        ", updateTime=" + updateTime +
+        ", updateUser=" + updateUser +
+        ", display=" + display +
+        ", deptId=" + deptId +
+        "}";
+    }
+}
---
+++ app/src/main/java/cn/atsoft/dasheng/app/model/params/ItemBrandBindParam.java
@@ -0,0 +1,75 @@
+package cn.atsoft.dasheng.app.model.params;
+
+import lombok.Data;
+import cn.atsoft.dasheng.model.validator.BaseValidatingParam;
+
+import java.io.Serializable;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * <p>
+ * 商品品牌绑定表
+ * </p>
+ *
+ * @author 
+ * @since 2021-09-24
+ */
+@Data
+@ApiModel
+public class ItemBrandBindParam implements Serializable, BaseValidatingParam {
+
+    private static final long serialVersionUID = 1L;
+
+
+    /**
+     * 产品品牌绑定表
+     */
+    @ApiModelProperty("产品品牌绑定表")
+    private Long itemBrandBindId;
+
+    /**
+     * 商品id
+     */
+    @ApiModelProperty("商品id")
+    private Long itemId;
+
+    /**
+     * 品牌id
+     */
+    @ApiModelProperty("品牌id")
+    private Long brandId;
+
+    @ApiModelProperty(hidden = true)
+    private Date createTime;
+
+    @ApiModelProperty(hidden = true)
+    private Long createUser;
+
+    @ApiModelProperty(hidden = true)
+    private Date updateTime;
+
+    @ApiModelProperty(hidden = true)
+    private Long updateUser;
+
+    @ApiModelProperty("")
+    private Integer display;
+
+    /**
+     * 部门编号
+     */
+    @ApiModelProperty("部门编号")
+    private Long deptId;
+
+    @ApiModelProperty("父ID顺序数组")
+    private List<String> pidValue;
+
+    @Override
+    public String checkParam() {
+        return null;
+    }
+
+}
