3dcb557c022f1dfde64cc7e2ac20a20e42c4cd76
song <2625598959@qq.com>
2021-09-26 09:14:17 +0800

    添加联系人  判断电话 如果重复 不让添加
    Signed-off-by: song <2625598959@qq.com>

--- app/src/main/java/cn/atsoft/dasheng/app/service/impl/ContactsServiceImpl.java
+++ app/src/main/java/cn/atsoft/dasheng/app/service/impl/ContactsServiceImpl.java
@@ -62,6 +62,20 @@ public class ContactsServiceImpl extends ServiceImpl<ContactsMapper, Contacts> i
         if (ToolUtil.isEmpty(param.getContactsName())) {
             throw new ServiceException(500, "请不要输入空的名字");
         }
+        //先判断电话是否重复 如果过有重复 方式在添加联系人
+        List<Long> phoneNumber = new ArrayList<>();
+        if (ToolUtil.isEmpty(param.getPhoneParams())) {
+            throw new ServiceException(500, "请添加联系系人电话");
+        }
+        for (PhoneParam phoneParam : param.getPhoneParams()) {
+            phoneNumber.add(phoneParam.getPhoneNumber());
+        }
+
+        Integer count = phoneNumber.size() == 0 ? 0 : phoneService.lambdaQuery().in(Phone::getPhoneNumber, phoneNumber).count();
+        if (count > 0) {
+            throw new ServiceException(500, "电话已经重复");
+        }
+
         //通过绑定表查询判断联系人是否重复
         List<Contacts> contacts = this.query()
                 .in("contacts_name", param.getContactsName())
@@ -119,7 +133,7 @@ public class ContactsServiceImpl extends ServiceImpl<ContactsMapper, Contacts> i
             Contacts entity = getEntity(param);
             param.setDisplay(0);
             QueryWrapper queryWrapper = new QueryWrapper<>();
-            queryWrapper.eq("contacts_id",param.getContactsId());
+            queryWrapper.eq("contacts_id", param.getContactsId());
             phoneService.remove(queryWrapper);
             this.update(param);
             return entity;
