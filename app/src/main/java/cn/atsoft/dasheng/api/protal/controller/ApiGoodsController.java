f56135132df427be2148a40fb15f10ce3c2db9ba
weiChen <2683941980@qq.com>
2021-10-19 19:16:28 +0800

    添加详情

--- app/src/main/java/cn/atsoft/dasheng/api/protal/controller/ApiGoodsController.java
+++ app/src/main/java/cn/atsoft/dasheng/api/protal/controller/ApiGoodsController.java
@@ -1,10 +1,16 @@
 package cn.atsoft.dasheng.api.protal.controller;
 
+import cn.atsoft.dasheng.base.auth.annotion.Permission;
+import cn.atsoft.dasheng.core.util.ToolUtil;
 import cn.atsoft.dasheng.model.response.ResponseData;
 import cn.atsoft.dasheng.portal.goods.entity.Goods;
+import cn.atsoft.dasheng.portal.goods.model.params.GoodsParam;
+import cn.atsoft.dasheng.portal.goods.model.result.GoodsResult;
 import cn.atsoft.dasheng.portal.goods.service.GoodsService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +29,22 @@ public class ApiGoodsController {
         QueryWrapper<Goods> goodsQueryWrapper = new QueryWrapper<>();
         List<Goods> list = goodsService.list(goodsQueryWrapper);
         return ResponseData.success(list);
+    }
+
+    /**
+     * 查看详情接口
+     *
+     * @author siqiang
+     * @Date 2021-08-19
+     */
+    @RequestMapping(value = "/detail", method = RequestMethod.POST)
+    @ApiOperation("详情")
+    public ResponseData<GoodsResult> detail(@RequestBody GoodsParam goodsParam) {
+        Goods detail = this.goodsService.getById(goodsParam.getGoodId());
+        GoodsResult result = new GoodsResult();
+        ToolUtil.copyProperties(detail, result);
+
+//        result.setValue(parentValue);
+        return ResponseData.success(result);
     }
 }
