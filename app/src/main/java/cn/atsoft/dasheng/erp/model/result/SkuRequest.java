9bf989a05c6941d0fb549ef7b02661a3af19dbf0
weiChen <2683941980@qq.com>
2021-10-19 08:25:36 +0800

    修改产品详细

--- app/src/main/java/cn/atsoft/dasheng/erp/model/result/SkuRequest.java
+++ app/src/main/java/cn/atsoft/dasheng/erp/model/result/SkuRequest.java
@@ -1,12 +1,13 @@
 package cn.atsoft.dasheng.erp.model.result;
 
 import cn.atsoft.dasheng.erp.entity.AttributeValues;
+import cn.atsoft.dasheng.erp.entity.ItemAttribute;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class SkuRequest {
-    private String attribute;
+    private ItemAttribute attribute;
     private List<AttributeValues> value;
 }
