3972850a0c91ff8db0b78e2938aa4cabade82a1c
song <2625598959@qq.com>
2021-10-26 08:54:22 +0800

    修改spu分类接口
    Signed-off-by: song <2625598959@qq.com>

--- app/src/main/java/cn/atsoft/dasheng/erp/service/impl/SpuClassificationServiceImpl.java
+++ app/src/main/java/cn/atsoft/dasheng/erp/service/impl/SpuClassificationServiceImpl.java
@@ -3,18 +3,22 @@ package cn.atsoft.dasheng.erp.service.impl;
 
 import cn.atsoft.dasheng.base.pojo.page.PageFactory;
 import cn.atsoft.dasheng.base.pojo.page.PageInfo;
+import cn.atsoft.dasheng.erp.entity.Spu;
 import cn.atsoft.dasheng.erp.entity.SpuClassification;
 import cn.atsoft.dasheng.erp.mapper.SpuClassificationMapper;
 import cn.atsoft.dasheng.erp.model.params.SpuClassificationParam;
 import cn.atsoft.dasheng.erp.model.result.SpuClassificationResult;
-import  cn.atsoft.dasheng.erp.service.SpuClassificationService;
+import cn.atsoft.dasheng.erp.service.SpuClassificationService;
 import cn.atsoft.dasheng.core.util.ToolUtil;
+import cn.atsoft.dasheng.erp.service.SpuService;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -28,19 +32,22 @@ import java.util.List;
 @Service
 public class SpuClassificationServiceImpl extends ServiceImpl<SpuClassificationMapper, SpuClassification> implements SpuClassificationService {
 
+    @Autowired
+    private SpuService spuService;
+
     @Override
-    public void add(SpuClassificationParam param){
+    public void add(SpuClassificationParam param) {
         SpuClassification entity = getEntity(param);
         this.save(entity);
     }
 
     @Override
-    public void delete(SpuClassificationParam param){
+    public void delete(SpuClassificationParam param) {
         this.removeById(getKey(param));
     }
 
     @Override
-    public void update(SpuClassificationParam param){
+    public void update(SpuClassificationParam param) {
         SpuClassification oldEntity = getOldEntity(param);
         SpuClassification newEntity = getEntity(param);
         ToolUtil.copyProperties(newEntity, oldEntity);
@@ -48,23 +55,24 @@ public class SpuClassificationServiceImpl extends ServiceImpl<SpuClassificationM
     }
 
     @Override
-    public SpuClassificationResult findBySpec(SpuClassificationParam param){
+    public SpuClassificationResult findBySpec(SpuClassificationParam param) {
         return null;
     }
 
     @Override
-    public List<SpuClassificationResult> findListBySpec(SpuClassificationParam param){
+    public List<SpuClassificationResult> findListBySpec(SpuClassificationParam param) {
         return null;
     }
 
     @Override
-    public PageInfo<SpuClassificationResult> findPageBySpec(SpuClassificationParam param){
+    public PageInfo<SpuClassificationResult> findPageBySpec(SpuClassificationParam param) {
         Page<SpuClassificationResult> pageContext = getPageContext();
         IPage<SpuClassificationResult> page = this.baseMapper.customPageList(pageContext, param);
+        format(page.getRecords());
         return PageFactory.createPageInfo(page);
     }
 
-    private Serializable getKey(SpuClassificationParam param){
+    private Serializable getKey(SpuClassificationParam param) {
         return param.getSpuClassificationId();
     }
 
@@ -82,4 +90,21 @@ public class SpuClassificationServiceImpl extends ServiceImpl<SpuClassificationM
         return entity;
     }
 
+    public void format(List<SpuClassificationResult> data) {
+        List<Long> ids = new ArrayList<>();
+        for (SpuClassificationResult datum : data) {
+            ids.add(datum.getSpuClassificationId());
+        }
+        List<Spu> spus = ids.size() == 0 ? new ArrayList<>() : spuService.lambdaQuery().in(Spu::getSpuClassificationId, ids).list();
+
+        for (SpuClassificationResult datum : data) {
+            List<Spu> spuList = new ArrayList<>();
+            for (Spu spu : spus) {
+                if (datum.getSpuClassificationId().equals(spu.getSpuClassificationId())) {
+                    spuList.add(spu);
+                }
+            }
+            datum.setSpuList(spuList);
+        }
+    }
 }
