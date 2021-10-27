d7cfaa53273bb39ff736a6823a529bf7ca636558
song <2625598959@qq.com>
2021-10-27 17:05:05 +0800

    重复添加验证
    Signed-off-by: song <2625598959@qq.com>

--- app/src/main/java/cn/atsoft/dasheng/erp/service/impl/QualityCheckClassificationServiceImpl.java
+++ app/src/main/java/cn/atsoft/dasheng/erp/service/impl/QualityCheckClassificationServiceImpl.java
@@ -7,8 +7,9 @@ import cn.atsoft.dasheng.erp.entity.QualityCheckClassification;
 import cn.atsoft.dasheng.erp.mapper.QualityCheckClassificationMapper;
 import cn.atsoft.dasheng.erp.model.params.QualityCheckClassificationParam;
 import cn.atsoft.dasheng.erp.model.result.QualityCheckClassificationResult;
-import  cn.atsoft.dasheng.erp.service.QualityCheckClassificationService;
+import cn.atsoft.dasheng.erp.service.QualityCheckClassificationService;
 import cn.atsoft.dasheng.core.util.ToolUtil;
+import cn.atsoft.dasheng.model.exception.ServiceException;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -29,18 +30,22 @@ import java.util.List;
 public class QualityCheckClassificationServiceImpl extends ServiceImpl<QualityCheckClassificationMapper, QualityCheckClassification> implements QualityCheckClassificationService {
 
     @Override
-    public void add(QualityCheckClassificationParam param){
+    public void add(QualityCheckClassificationParam param) {
+        Integer count = this.query().eq("name", param.getName()).count();
+        if (count > 0) {
+            throw new ServiceException(500, "已有重复分类");
+        }
         QualityCheckClassification entity = getEntity(param);
         this.save(entity);
     }
 
     @Override
-    public void delete(QualityCheckClassificationParam param){
+    public void delete(QualityCheckClassificationParam param) {
         this.removeById(getKey(param));
     }
 
     @Override
-    public void update(QualityCheckClassificationParam param){
+    public void update(QualityCheckClassificationParam param) {
         QualityCheckClassification oldEntity = getOldEntity(param);
         QualityCheckClassification newEntity = getEntity(param);
         ToolUtil.copyProperties(newEntity, oldEntity);
@@ -48,23 +53,23 @@ public class QualityCheckClassificationServiceImpl extends ServiceImpl<QualityCh
     }
 
     @Override
-    public QualityCheckClassificationResult findBySpec(QualityCheckClassificationParam param){
+    public QualityCheckClassificationResult findBySpec(QualityCheckClassificationParam param) {
         return null;
     }
 
     @Override
-    public List<QualityCheckClassificationResult> findListBySpec(QualityCheckClassificationParam param){
+    public List<QualityCheckClassificationResult> findListBySpec(QualityCheckClassificationParam param) {
         return null;
     }
 
     @Override
-    public PageInfo<QualityCheckClassificationResult> findPageBySpec(QualityCheckClassificationParam param){
+    public PageInfo<QualityCheckClassificationResult> findPageBySpec(QualityCheckClassificationParam param) {
         Page<QualityCheckClassificationResult> pageContext = getPageContext();
         IPage<QualityCheckClassificationResult> page = this.baseMapper.customPageList(pageContext, param);
         return PageFactory.createPageInfo(page);
     }
 
-    private Serializable getKey(QualityCheckClassificationParam param){
+    private Serializable getKey(QualityCheckClassificationParam param) {
         return param.getQualityCheckClassificationId();
     }
 
