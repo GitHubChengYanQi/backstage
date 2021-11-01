359dc2696e22871d97b104a9d40a51aaeeeca2eb
weiChen <2683941980@qq.com>
2021-11-01 18:07:07 +0800

    添加判断

--- app/src/main/java/cn/atsoft/dasheng/erp/service/impl/SpuServiceImpl.java
+++ app/src/main/java/cn/atsoft/dasheng/erp/service/impl/SpuServiceImpl.java
@@ -86,10 +86,6 @@ public class SpuServiceImpl extends ServiceImpl<SpuMapper, Spu> implements SpuSe
         Spu entity = getEntity(param);
 
 
-        if (ToolUtil.isNotEmpty(param.getSpuAttributes().getSpuRequests())) {
-            String toJSONString = JSON.toJSONString(param.getSpuAttributes().getSpuRequests());
-            entity.setAttribute(toJSONString);
-        }
 
         if (param.getIsHidden()) {
             SpuClassificationParam spuClassificationParam = new SpuClassificationParam();
@@ -103,6 +99,10 @@ public class SpuServiceImpl extends ServiceImpl<SpuMapper, Spu> implements SpuSe
         }else{
             if (ToolUtil.isEmpty(param.getSpuAttributes().getSpuRequests())) {
                 throw new ServiceException(500, "填入信息不完整");
+            }
+            if (ToolUtil.isNotEmpty(param.getSpuAttributes().getSpuRequests())) {
+                String toJSONString = JSON.toJSONString(param.getSpuAttributes().getSpuRequests());
+                entity.setAttribute(toJSONString);
             }
             List<List<String>> result = new ArrayList<List<String>>();
 //        param.getSpuAttributes().getSpuRequests().sort((x, y) -> x.getAttributeId().compareTo(y.getAttributeId()));
@@ -368,10 +368,12 @@ public class SpuServiceImpl extends ServiceImpl<SpuMapper, Spu> implements SpuSe
         List<SpuClassification> spuClassifications = classIds.size() == 0 ? new ArrayList<>() : spuClassificationService.query().in("spu_classification_id", classIds).list();
 
         for (SpuResult spuResult : param) {
-            for (Category category : categoryList) {
-                if (spuResult.getCategoryId().equals(category.getCategoryId())) {
-                    spuResult.setCategory(category);
-                    break;
+            if (ToolUtil.isNotEmpty(spuResult.getCategoryId())){
+                for (Category category : categoryList) {
+                    if (spuResult.getCategoryId().equals(category.getCategoryId())) {
+                        spuResult.setCategory(category);
+                        break;
+                    }
                 }
             }
             for (Unit unit : units) {
