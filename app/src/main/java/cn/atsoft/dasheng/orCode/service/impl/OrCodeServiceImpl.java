c9bd9f763b2d0c12e7d2668a899944f31eca0065
song <2625598959@qq.com>
2021-11-11 10:38:35 +0800

    修改绑定
    Signed-off-by: song <2625598959@qq.com>

--- app/src/main/java/cn/atsoft/dasheng/orCode/service/impl/OrCodeServiceImpl.java
+++ app/src/main/java/cn/atsoft/dasheng/orCode/service/impl/OrCodeServiceImpl.java
@@ -370,7 +370,11 @@ public class OrCodeServiceImpl extends ServiceImpl<OrCodeMapper, OrCode> impleme
     public Boolean judgeBind(InKindRequest inKindRequest) {
         OrCodeBind orCodeBind = orCodeBindService.query().eq("qr_code_id", inKindRequest.getCodeId()).one();
         if (ToolUtil.isNotEmpty(orCodeBind) && orCodeBind.getSource().equals("item")) {
-            Inkind one = inkindService.query().eq("inkind_id", orCodeBind.getFormId()).eq("sku_id", inKindRequest.getId()).one();
+            Inkind one = inkindService.query().eq("inkind_id", orCodeBind.getFormId()).eq("sku_id", inKindRequest.getId())
+                    .eq("brand_id", inKindRequest.getBrandId())
+                    .eq("selling_price", inKindRequest.getSellingPrice())
+                    .eq("cost_price", inKindRequest.getCostPrice())
+                    .one();
             if (ToolUtil.isEmpty(one)) {
                 return false;
             }
