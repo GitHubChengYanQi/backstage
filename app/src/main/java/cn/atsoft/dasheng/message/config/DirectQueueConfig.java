99050a315beeaa43d5c8eefa20d942b87c071379
Sing <78276478@qq.com>
2021-11-12 16:42:17 +0800

    使用消息队列发送消息

---
+++ app/src/main/java/cn/atsoft/dasheng/message/config/DirectQueueConfig.java
@@ -0,0 +1,73 @@
+package cn.atsoft.dasheng.message.config;
+
+import com.google.common.collect.Maps;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+@Configuration
+public class DirectQueueConfig {
+
+    protected static final Logger logger = LoggerFactory.getLogger(DirectQueueConfig.class);
+
+    public final static String MESSAGE_REAL_EXCHANGE = "message.real.topicExchange";
+    public final static String MESSAGE_REAL_ROUTE = "message.real.route";
+    public final static String MESSAGE_REAL_QUEUE = "message.real.topicExchange";
+
+    public final static String MESSAGE_DELAY_EXCHANGE = "message.delay.topicExchange";
+    public final static String MESSAGE_DELAY_ROUTE = "message.delay.route";
+    public final static String MESSAGE_DELAY_QUEUE = "message.delay.topicExchange";
+
+    /**
+     * 声明队列
+     * @return Queue
+     */
+    @Bean
+    public Queue messageQueue(){
+        return new Queue(MESSAGE_REAL_QUEUE);
+    }
+
+    @Bean
+    public Queue messageDelayQueue(){
+
+        Map<String, Object> argsMap= Maps.newHashMap();
+        argsMap.put("x-dead-letter-exchange",MESSAGE_REAL_EXCHANGE); //真正的交换机
+        argsMap.put("x-dead-letter-routing-key",MESSAGE_REAL_ROUTE); //真正的路由键
+        return new Queue(MESSAGE_DELAY_QUEUE,true,false,false,argsMap);
+    }
+
+    /**
+     * 声明交换机
+     * @return TopicExchange
+     */
+    @Bean
+    public TopicExchange topicExchange(){
+        return new TopicExchange(MESSAGE_REAL_EXCHANGE);
+    }
+
+    @Bean
+    public TopicExchange topicDelayExchange(){
+        return new TopicExchange(MESSAGE_DELAY_EXCHANGE);
+    }
+    /**
+     * 绑定交换机
+     */
+    @Bean
+    Binding bindingTopicExchange(){
+        logger.info("绑定队列");
+        return BindingBuilder.bind(messageQueue()).to(topicExchange()).with(MESSAGE_REAL_ROUTE);
+    }
+
+    @Bean
+    Binding bindingTopicDelayExchange(){
+        logger.info("绑定延迟队列");
+        return BindingBuilder.bind(messageDelayQueue()).to(topicDelayExchange()).with(MESSAGE_DELAY_ROUTE);
+    }
+}
---
+++ app/src/main/java/cn/atsoft/dasheng/message/topic/TopicMessage.java
@@ -0,0 +1,36 @@
+package cn.atsoft.dasheng.message.topic;
+
+import cn.atsoft.dasheng.message.entity.MessageEntity;
+import com.alibaba.fastjson.JSON;
+import com.rabbitmq.client.Channel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+import static cn.atsoft.dasheng.message.config.DirectQueueConfig.MESSAGE_REAL_QUEUE;
+
+@Component
+public class TopicMessage {
+
+    protected static final Logger logger = LoggerFactory.getLogger(TopicMessage.class);
+
+    @RabbitListener(queues = MESSAGE_REAL_QUEUE)
+    public void readMessage(Message message, Channel channel) throws IOException {
+        channel.basicAck(message.getMessageProperties().getDeliveryTag(),false);
+//        logger.info(new String(message.getBody()));
+        MessageEntity messageEntity = JSON.parseObject(message.getBody(),MessageEntity.class);
+        switch (messageEntity.getType()){
+            case CP:
+                break;
+            case MP:
+                break;
+            case SMS:
+                break;
+            default:
+        }
+    }
+}
