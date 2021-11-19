97fee54d830d9cac1ecc4d32a408559130967a06
song <2625598959@qq.com>
2021-11-19 14:29:09 +0800

    递归
    Signed-off-by: song <2625598959@qq.com>

--- base-support/form/src/main/java/cn/atsoft/dasheng/form/model/params/ActivitiAuditParam.java
+++ base-support/form/src/main/java/cn/atsoft/dasheng/form/model/params/ActivitiAuditParam.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import cn.atsoft.dasheng.model.validator.BaseValidatingParam;
 
 import java.io.Serializable;
+
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
@@ -24,7 +25,9 @@ public class ActivitiAuditParam implements Serializable, BaseValidatingParam {
 
     private static final long serialVersionUID = 1L;
 
+    private ActivitiStepsParam activitiStepsParam;
 
+    private Long processId;
     /**
      * 主键
      */
--- base-support/form/src/main/java/cn/atsoft/dasheng/form/service/impl/ActivitiProcessServiceImpl.java
+++ base-support/form/src/main/java/cn/atsoft/dasheng/form/service/impl/ActivitiProcessServiceImpl.java
@@ -3,15 +3,18 @@ package cn.atsoft.dasheng.form.service.impl;
 
 import cn.atsoft.dasheng.base.pojo.page.PageFactory;
 import cn.atsoft.dasheng.base.pojo.page.PageInfo;
+import cn.atsoft.dasheng.form.entity.ActivitiAudit;
 import cn.atsoft.dasheng.form.entity.ActivitiProcess;
 import cn.atsoft.dasheng.form.mapper.ActivitiProcessMapper;
 import cn.atsoft.dasheng.form.model.params.ActivitiProcessParam;
 import cn.atsoft.dasheng.form.model.result.ActivitiProcessResult;
-import  cn.atsoft.dasheng.form.service.ActivitiProcessService;
+import cn.atsoft.dasheng.form.service.ActivitiAuditService;
+import cn.atsoft.dasheng.form.service.ActivitiProcessService;
 import cn.atsoft.dasheng.core.util.ToolUtil;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.Serializable;
@@ -27,20 +30,22 @@ import java.util.List;
  */
 @Service
 public class ActivitiProcessServiceImpl extends ServiceImpl<ActivitiProcessMapper, ActivitiProcess> implements ActivitiProcessService {
+    @Autowired
+    private ActivitiAuditService auditService;
 
     @Override
-    public void add(ActivitiProcessParam param){
+    public void add(ActivitiProcessParam param) {
         ActivitiProcess entity = getEntity(param);
         this.save(entity);
     }
 
     @Override
-    public void delete(ActivitiProcessParam param){
+    public void delete(ActivitiProcessParam param) {
         this.removeById(getKey(param));
     }
 
     @Override
-    public void update(ActivitiProcessParam param){
+    public void update(ActivitiProcessParam param) {
         ActivitiProcess oldEntity = getOldEntity(param);
         ActivitiProcess newEntity = getEntity(param);
         ToolUtil.copyProperties(newEntity, oldEntity);
@@ -48,23 +53,23 @@ public class ActivitiProcessServiceImpl extends ServiceImpl<ActivitiProcessMappe
     }
 
     @Override
-    public ActivitiProcessResult findBySpec(ActivitiProcessParam param){
+    public ActivitiProcessResult findBySpec(ActivitiProcessParam param) {
         return null;
     }
 
     @Override
-    public List<ActivitiProcessResult> findListBySpec(ActivitiProcessParam param){
+    public List<ActivitiProcessResult> findListBySpec(ActivitiProcessParam param) {
         return null;
     }
 
     @Override
-    public PageInfo<ActivitiProcessResult> findPageBySpec(ActivitiProcessParam param){
+    public PageInfo<ActivitiProcessResult> findPageBySpec(ActivitiProcessParam param) {
         Page<ActivitiProcessResult> pageContext = getPageContext();
         IPage<ActivitiProcessResult> page = this.baseMapper.customPageList(pageContext, param);
         return PageFactory.createPageInfo(page);
     }
 
-    private Serializable getKey(ActivitiProcessParam param){
+    private Serializable getKey(ActivitiProcessParam param) {
         return param.getProcessId();
     }
 
