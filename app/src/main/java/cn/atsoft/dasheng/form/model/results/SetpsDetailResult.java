93c0c50b9641d8d57d01f362c6111f1afc5a8fda
weiChen <2683941980@qq.com>
2021-11-25 16:35:21 +0800

    修改审批详情接口

--- app/src/main/java/cn/atsoft/dasheng/form/model/results/SetpsDetailResult.java
+++ app/src/main/java/cn/atsoft/dasheng/form/model/results/SetpsDetailResult.java
@@ -1,6 +1,7 @@
 package cn.atsoft.dasheng.form.model.results;
 
 import cn.atsoft.dasheng.erp.model.result.QualityTaskResult;
+import cn.atsoft.dasheng.form.entity.ActivitiProcess;
 import cn.atsoft.dasheng.form.model.result.ActivitiAuditResult;
 import lombok.Data;
 
@@ -9,6 +10,7 @@ import java.util.List;
 @Data
 public class SetpsDetailResult {
     private ActivitiAuditResult auditResult;
+    private ActivitiProcess activitiProcess;
     private QualityTaskResult qualityTaskResult;
     private List<ActivitiAuditResult> auditResults;
 }
