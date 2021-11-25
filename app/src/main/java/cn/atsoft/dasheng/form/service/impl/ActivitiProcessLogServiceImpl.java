0e5950ec5400ee7ec864ca851266af7177ad8634
weiChen <2683941980@qq.com>
2021-11-25 17:22:18 +0800

    修改权限

--- app/src/main/java/cn/atsoft/dasheng/form/service/impl/ActivitiProcessLogServiceImpl.java
+++ app/src/main/java/cn/atsoft/dasheng/form/service/impl/ActivitiProcessLogServiceImpl.java
@@ -75,15 +75,19 @@ public class ActivitiProcessLogServiceImpl extends ServiceImpl<ActivitiProcessLo
 //          }
         ActivitiSteps steps = stepsService.getById(param.getSetpsId());
         ActivitiProcess process = processService.getById(steps.getProcessId());
+
         ActivitiAudit audit = auditService.query().eq("setps_id", steps.getChildren()).one();
+        ActivitiAudit nowAudit = auditService.query().eq("setps_id", steps.getSetpsId()).one();
         Long userId = LoginContextHolder.getContext().getUserId();
-        for (StartUsers.Users user : audit.getRule().getStartUsers().getUsers()) {
-            if (!user.getKey().equals(userId)) {
-                throw new ServiceException(500,"您没有权限操作该审批任务");
-            }
-        }
 
-
+//        if (ToolUtil.isNotEmpty(nowAudit.getRule()) && ToolUtil.isNotEmpty(nowAudit.getRule().getStartUsers()) && ToolUtil.isNotEmpty(nowAudit.getRule().getStartUsers().getUsers())) {
+//            for (StartUsers.Users user : audit.getRule().getStartUsers().getUsers()) {
+//
+//                if (!user.getKey()!=(userId)) {
+//                    throw new ServiceException(500,"您没有权限操作该审批任务");
+//                }
+//            }
+//        }
 
 
         ActivitiProcessTask activitiProcessTask = activitiProcessTaskService.query().eq("form_id", param.getFormId()).one();
@@ -94,16 +98,15 @@ public class ActivitiProcessLogServiceImpl extends ServiceImpl<ActivitiProcessLo
 
         if (entity.getStatus() == 1) {
             taskSend.logAddSend(audit.getType(), audit.getRule(), process.getUrl(), steps.getChildren(), activitiProcessTask.getFormId());
-        }else {
+        } else {
 
             List<QualityTask> qualityTasks = qualityTaskService.query().eq("quality_task_id", activitiProcessTask.getFormId()).list();
-            if (qualityTasks.size()>0){
+            if (qualityTasks.size() > 0) {
                 taskSend.vetoSend(audit.getType(), process.getUrl(), steps.getChildren(), activitiProcessTask.getFormId());
             }
         }
 //
     }
-
 
 
     @Override
