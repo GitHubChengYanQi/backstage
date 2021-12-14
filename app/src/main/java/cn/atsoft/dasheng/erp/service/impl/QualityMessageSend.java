ca0db959d956d13df99398f19382ba7f894d501b
captain_jazz <captain_jazz@foxmail.com>
2021-12-14 08:22:42 +0800

    完成质检节点更新推送

--- app/src/main/java/cn/atsoft/dasheng/erp/service/impl/QualityMessageSend.java
+++ app/src/main/java/cn/atsoft/dasheng/erp/service/impl/QualityMessageSend.java
@@ -2,6 +2,7 @@ package cn.atsoft.dasheng.erp.service.impl;
 
 import cn.atsoft.dasheng.erp.config.MobileService;
 import cn.atsoft.dasheng.erp.entity.QualityTask;
+import cn.atsoft.dasheng.erp.model.params.QualityTaskParam;
 import cn.atsoft.dasheng.erp.service.AuditMessageSend;
 import cn.atsoft.dasheng.erp.service.QualityTaskService;
 import cn.atsoft.dasheng.form.entity.ActivitiProcessTask;
@@ -14,7 +15,11 @@ import cn.atsoft.dasheng.sys.modular.system.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class QualityMessageSend implements AuditMessageSend {
 
@@ -27,6 +32,8 @@ public class QualityMessageSend implements AuditMessageSend {
     @Autowired
     private ActivitiProcessTaskService activitiProcessTaskService;
 
+    @Autowired
+    private MobileService mobileService;
     @Override
     public void send(Long taskId,RuleType type, List<Long> users,String url,String createName) {
         ActivitiProcessTask processTask = activitiProcessTaskService.getById(taskId);
@@ -49,6 +56,33 @@ public class QualityMessageSend implements AuditMessageSend {
         wxCpSendTemplate.setWxCpTemplate(wxCpTemplate);
         wxCpSendTemplate.sendTemplate();
 
+    }
+    public void dispatchSend(Long parentId, QualityTaskParam qualityTaskParam){
+        WxCpTemplate wxCpTemplate = new WxCpTemplate();
+        String userIds = qualityTaskParam.getUserIds();
+        List<Long> users = Arrays.asList(userIds.split(",")).stream().map(s -> Long.parseLong(s.trim())).collect(Collectors.toList());
+        wxCpTemplate.setUserIds(users);
+        String url = mobileService.getMobileConfig().getUrl();
+        url = url + "/#/Work/Quality?id=" + parentId;
+        wxCpTemplate.setUrl(url);
+        wxCpTemplate.setDescription("点击查看新质检任务");
+        wxCpTemplate.setTitle("您被分派新的任务");
+        wxCpTemplate.setType(1);
+        wxCpSendTemplate.setWxCpTemplate(wxCpTemplate);
+        wxCpSendTemplate.sendTemplate();
+    }
+    public void refuseQuality(){
+        WxCpTemplate wxCpTemplate = new WxCpTemplate();
+        List<Long> users = new ArrayList<>();
+        wxCpTemplate.setUserIds(users);
+        String url = mobileService.getMobileConfig().getUrl();
+        url = url + "/#/Work/Quality?id=" + "?";
+        wxCpTemplate.setUrl(url);
+        wxCpTemplate.setDescription("质检任务有物料被拒绝质检");
+        wxCpTemplate.setTitle("质检任务物料被拒绝质检");
+        wxCpTemplate.setType(1);
+        wxCpSendTemplate.setWxCpTemplate(wxCpTemplate);
+        wxCpSendTemplate.sendTemplate();
     }
 
 }
