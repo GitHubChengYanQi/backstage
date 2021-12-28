9518346e7c585f6b62eab2a30f018d34fa494156
captain_jazz <captain_jazz@foxmail.com>
2021-12-28 13:29:10 +0800

    增加打印模板功能

---
+++ app/src/main/java/cn/atsoft/dasheng/printTemplate/model/params/PrintTemplateParam.java
@@ -0,0 +1,96 @@
+package cn.atsoft.dasheng.printTemplate.model.params;
+
+import lombok.Data;
+import cn.atsoft.dasheng.model.validator.BaseValidatingParam;
+
+import java.io.Serializable;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * <p>
+ * 编辑模板
+ * </p>
+ *
+ * @author Captain_Jazz
+ * @since 2021-12-28
+ */
+@Data
+@ApiModel
+public class PrintTemplateParam implements Serializable, BaseValidatingParam {
+
+    private static final long serialVersionUID = 1L;
+
+
+    /**
+     * 编辑模板id
+     */
+    @ApiModelProperty("编辑模板id")
+    private Long printTemplateId;
+
+    /**
+     * 类型
+     */
+    @ApiModelProperty("类型")
+    private String type;
+
+    /**
+     * 名称
+     */
+    @ApiModelProperty("名称")
+    private String name;
+
+    /**
+     * 模板
+     */
+    @ApiModelProperty("模板")
+    private String templete;
+
+    /**
+     * 创建时间
+     */
+    @ApiModelProperty(hidden = true)
+    private Date createTime;
+
+    /**
+     * 创建者
+     */
+    @ApiModelProperty(hidden = true)
+    private Long createUser;
+
+    /**
+     * 修改时间
+     */
+    @ApiModelProperty(hidden = true)
+    private Date updateTime;
+
+    /**
+     * 修改者
+     */
+    @ApiModelProperty(hidden = true)
+    private Long updateUser;
+
+    /**
+     * 状态
+     */
+    @ApiModelProperty("状态")
+    private Integer display;
+
+    /**
+     * 部门编号
+     */
+    @ApiModelProperty("部门编号")
+    private Long deptId;
+
+    @ApiModelProperty("父ID顺序数组")
+    private List<String> pidValue;
+
+    @Override
+    public String checkParam() {
+        return null;
+    }
+
+}
---
+++ app/src/main/java/cn/atsoft/dasheng/printTemplate/wrapper/PrintTemplateSelectWrapper.java
@@ -0,0 +1,19 @@
+package cn.atsoft.dasheng.printTemplate.wrapper;
+
+import cn.atsoft.dasheng.core.base.warpper.BaseControllerWrapper;
+import cn.hutool.core.convert.Convert;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class PrintTemplateSelectWrapper extends BaseControllerWrapper {
+
+    public PrintTemplateSelectWrapper(List<Map<String, Object>> multi) {
+        super(multi);
+    }
+
+    @Override
+    protected void wrapTheMap(Map<String, Object> map) {
+    }
+}
