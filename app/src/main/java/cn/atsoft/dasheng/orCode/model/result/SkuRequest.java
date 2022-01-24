d571899de31835e17c6d025cb622444388b85d88
weiChen <2683941980@qq.com>
2022-01-24 08:43:31 +0800

    修改二维码返回数据

--- app/src/main/java/cn/atsoft/dasheng/orCode/model/result/SkuRequest.java
+++ app/src/main/java/cn/atsoft/dasheng/orCode/model/result/SkuRequest.java
@@ -1,11 +1,10 @@
 package cn.atsoft.dasheng.orCode.model.result;
 
 import cn.atsoft.dasheng.erp.model.result.SkuResult;
-import cn.atsoft.dasheng.erp.model.result.SpuResult;
 import lombok.Data;
 
 @Data
-public class SpuRequest {
+public class SkuRequest {
     private String type;
-    private SpuResult result;
+    private SkuResult result;
 }
