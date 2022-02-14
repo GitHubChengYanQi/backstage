4bb74bbebe9668bc93eda4188cd23bd6e04ad3a3
song <2625598959@qq.com>
2022-02-14 08:26:45 +0800

    导入模板
    Signed-off-by: song <2625598959@qq.com>

--- app/src/main/java/cn/atsoft/dasheng/crm/controller/CrmExcelController.java
+++ app/src/main/java/cn/atsoft/dasheng/crm/controller/CrmExcelController.java
@@ -81,7 +81,6 @@ public class CrmExcelController {
     private BrandService brandService;
 
 
-
     /**
      * 上传excel填报
      */
@@ -341,6 +340,60 @@ public class CrmExcelController {
         //workbook将Excel写入到response的输出流中，供页面下载
         workbook.write(response.getOutputStream());
 //        System.out.println(workbook.write(response.getOutputStream()));
+    }
+
+    @RequestMapping(value = "/SkuExcel", method = RequestMethod.GET)
+    @ApiOperation("导出")
+    public void SkuExcel(HttpServletResponse response, Long type, String url) throws IOException {
+
+        String[] header = {"成品码", "分类", "产品", "型号", "单位", "是否批量", "规则名称"};
+        HSSFWorkbook workbook = new HSSFWorkbook();
+        HSSFSheet sheet = workbook.createSheet("二维码导出");
+//        sheet.setDefaultColumnWidth(40);
+//        CellRangeAddress region = new CellRangeAddress(0, 0, 0, 1);
+//        sheet.addMergedRegion(region);
+//        sheet.setColumnWidth(0, 10);
+        HSSFRow titleRow = sheet.createRow(0);
+        HSSFCell ti = titleRow.createCell(0);
+
+        HSSFCellStyle titleStyle = workbook.createCellStyle();
+        titleStyle.setFillForegroundColor(IndexedColors.LIGHT_GREEN.index);
+        titleStyle.setAlignment(HorizontalAlignment.CENTER);
+        titleStyle.setVerticalAlignment(VerticalAlignment.CENTER);
+        ti.setCellStyle(titleStyle);
+
+
+        HSSFRow headrow = sheet.createRow(0);
+
+        for (int i = 0; i < header.length; i++) {
+            //创建一个单元格
+            HSSFCell cell = headrow.createCell(i);
+
+            //创建一个内容对象
+            HSSFRichTextString text = new HSSFRichTextString(header[i]);
+
+
+            HSSFCellStyle headerStyle = workbook.createCellStyle();
+            headerStyle.setFillForegroundColor(IndexedColors.LIGHT_GREEN.index);
+            headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+            //将内容对象的文字内容写入到单元格中
+            cell.setCellValue(text);
+            cell.setCellStyle(headerStyle);
+        }
+
+        //准备将Excel的输出流通过response输出到页面下载
+        //八进制输出流
+        response.setContentType("application/octet-stream");
+
+        //这后面可以设置导出Excel的名称
+        response.setHeader("Content-disposition", "attachment;filename=物料导入模板.xls");
+
+        //刷新缓冲
+        response.flushBuffer();
+
+        //workbook将Excel写入到response的输出流中，供页面下载
+        workbook.write(response.getOutputStream());
     }
 }
 
