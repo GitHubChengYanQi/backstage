82a7e5ab0c97b060b94469f4d13406e73c58b80b
weiChen <2683941980@qq.com>
2022-02-26 10:12:43 +0800

    修改字段

--- app/src/main/java/cn/atsoft/dasheng/app/service/impl/CustomerServiceImpl.java
+++ app/src/main/java/cn/atsoft/dasheng/app/service/impl/CustomerServiceImpl.java
@@ -372,10 +372,22 @@ public class CustomerServiceImpl extends ServiceImpl<CustomerMapper, Customer> i
                     ToolUtil.copyProperties(invoice, invoiceResult);
 //                    record.setInvoiceResult(invoiceResult);
                     invoiceResults.add(invoiceResult);
+//                    break;
+                }
+            }
+
+
+
+            for (Invoice invoice : invoices) {      //对比开票
+                if (invoice.getInvoiceId().equals(record.getInvoiceId())) {
+                    InvoiceResult invoiceResult = new InvoiceResult();
+                    ToolUtil.copyProperties(invoice, invoiceResult);
+                    record.setInvoiceResult(invoiceResult);
+//                    invoiceResults.add(invoiceResult);
                     break;
                 }
             }
-            record.setInvoiceResults(invoiceResults);
+//            record.setInvoiceResults(invoiceResults);
             for (Adress adress : adresses) {
                 if (adress.getAdressId().equals(record.getDefaultAddress())) {
                     record.setAddress(adress);
