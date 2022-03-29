569cd9f7653e38b21cb437339f3c272737250c60
song <2625598959@qq.com>
2022-03-29 18:23:10 +0800

    测试  导出

--- app/src/main/java/cn/atsoft/dasheng/Excel/ContractExcel.java
+++ app/src/main/java/cn/atsoft/dasheng/Excel/ContractExcel.java
@@ -1,12 +1,13 @@
 package cn.atsoft.dasheng.Excel;
 
 
-
+import cn.afterturn.easypoi.exception.word.enmus.WordExportEnum;
 import cn.afterturn.easypoi.word.WordExportUtil;
 import cn.atsoft.dasheng.app.entity.Contract;
 import cn.atsoft.dasheng.app.service.ContractService;
 import cn.atsoft.dasheng.model.response.ResponseData;
 import cn.hutool.core.lang.Assert;
+import cn.hutool.poi.word.Word07Writer;
 import io.lettuce.core.dynamic.annotation.Param;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.poifs.filesystem.DirectoryEntry;
@@ -28,6 +29,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.Map;
 
 
@@ -113,25 +115,39 @@ public class ContractExcel {
 
     }
 
+    @RequestMapping(value = "export", method = RequestMethod.GET)
+    public void export(HttpServletRequest request, HttpServletResponse response, Long id) {
+        Contract contract = contractService.getById(id);
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("title", "这是标题");
+        params.put("name", "李四");
+        //这里是我说的一行代码
+        exportWord(contract.getContent(), "F:/test", "aaa.docx", params, request, response);
+    }
+
+
     /**
      * 导出word
      * <p>第一步生成替换后的word文件，只支持docx</p>
      * <p>第二步下载生成的文件</p>
      * <p>第三步删除生成的临时文件</p>
      * 模版变量中变量格式：{{foo}}
+     *
      * @param templatePath word模板地址
-     * @param temDir 生成临时文件存放地址
-     * @param fileName 文件名
-     * @param params 替换的参数
-     * @param request HttpServletRequest
-     * @param response HttpServletResponse
+     * @param temDir       生成临时文件存放地址
+     * @param fileName     文件名
+     * @param params       替换的参数
+     * @param request      HttpServletRequest
+     * @param response     HttpServletResponse
      */
+
     public static void exportWord(String templatePath, String temDir, String fileName, Map<String, Object> params, HttpServletRequest request, HttpServletResponse response) {
-        Assert.notNull(templatePath,"模板路径不能为空");
-        Assert.notNull(temDir,"临时文件路径不能为空");
-        Assert.notNull(fileName,"导出文件名不能为空");
-        Assert.isTrue(fileName.endsWith(".docx"),"word导出请使用docx格式");
-        if (!temDir.endsWith("/")){
+        Assert.notNull(templatePath, "模板路径不能为空");
+        Assert.notNull(temDir, "临时文件路径不能为空");
+        Assert.notNull(fileName, "导出文件名不能为空");
+        Assert.isTrue(fileName.endsWith(".docx"), "word导出请使用docx格式");
+        if (!temDir.endsWith("/")) {
             temDir = temDir + File.separator;
         }
         File dir = new File(temDir);
