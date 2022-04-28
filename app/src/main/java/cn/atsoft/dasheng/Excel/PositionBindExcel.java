b7ce8db1fe3385dff6f22bfe5c1948e8616ba781
song <2625598959@qq.com>
2022-04-28 08:33:57 +0800

    异步
    Signed-off-by: song <2625598959@qq.com>

--- app/src/main/java/cn/atsoft/dasheng/Excel/PositionBindExcel.java
+++ app/src/main/java/cn/atsoft/dasheng/Excel/PositionBindExcel.java
@@ -24,15 +24,22 @@ import cn.atsoft.dasheng.sys.modular.system.service.FileInfoService;
 import cn.hutool.poi.excel.ExcelReader;
 import cn.hutool.poi.excel.ExcelUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.hssf.usermodel.*;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.VerticalAlignment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -42,24 +49,13 @@ import java.util.List;
 @RequestMapping("/Excel")
 @Slf4j
 public class PositionBindExcel {
-    @Autowired
-    private StorehousePositionsBindService bindService;
-    @Autowired
-    private StorehousePositionsService positionsService;
-    @Autowired
-    private SkuService skuService;
-    @Autowired
-    private StockDetailsService detailsService;
-    @Autowired
-    private InkindService inkindService;
-    @Autowired
-    private OrCodeService codeService;
-    @Autowired
-    private BrandService brandService;
+
     @Autowired
     private FileInfoService fileInfoService;
     @Autowired
     private ExcelAsync excelAsync;
+    @Autowired
+    private SkuExcelService skuExcelService;
 
     protected static final Logger logger = LoggerFactory.getLogger(SkuExcelController.class);
 
@@ -81,7 +77,6 @@ public class PositionBindExcel {
         reader.addHeaderAlias("品牌", "brand");
 
 
-
         List<PositionBind> excels = reader.readAll(PositionBind.class);
         /**
          * 调用异步
@@ -91,5 +86,59 @@ public class PositionBindExcel {
         return ResponseData.success("ok");
     }
 
+    @RequestMapping(value = "/positionTemp", method = RequestMethod.GET)
+    public void spuTemp(HttpServletResponse response) {
 
+        String[] header = {"物料编码", "分类", "产品", "型号","库存余额","上级库位","库位","品牌"};
+
+        HSSFWorkbook workbook = new HSSFWorkbook();
+        HSSFSheet hssfSheet = workbook.createSheet("库位绑定模板");
+        HSSFSheet sheet = skuExcelService.dataEffective(hssfSheet, 300);
+        HSSFRow titleRow = sheet.createRow(0);
+        HSSFCell ti = titleRow.createCell(0);
+
+        HSSFCellStyle titleStyle = workbook.createCellStyle();
+        titleStyle.setFillForegroundColor((short) 10);
+
+        titleStyle.setAlignment(HorizontalAlignment.CENTER);
+        titleStyle.setVerticalAlignment(VerticalAlignment.CENTER);
+        ti.setCellStyle(titleStyle);
+
+        HSSFRow headRow = sheet.createRow(0);
+
+        for (int i = 0; i < header.length; i++) {
+
+            //创建一个单元格
+            HSSFCell cell = headRow.createCell(i);
+
+            //创建一个内容对象
+            HSSFRichTextString text = new HSSFRichTextString(header[i]);
+
+
+            HSSFCellStyle headerStyle = workbook.createCellStyle();
+            headerStyle.setFillForegroundColor(IndexedColors.LIGHT_GREEN.index);
+            headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+            //将内容对象的文字内容写入到单元格中
+            cell.setCellValue(text);
+            cell.setCellStyle(headerStyle);
+        }
+        //准备将Excel的输出流通过response输出到页面下载
+        //八进制输出流
+        response.setContentType("application/octet-stream");
+
+        //这后面可以设置导出Excel的名称
+        response.setHeader("Content-disposition", "attachment;filename=PositionTemp.xls");
+
+
+        try {
+            //刷新缓冲
+            response.flushBuffer();
+            //workbook将Excel写入到response的输出流中，供页面下载
+            workbook.write(response.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
