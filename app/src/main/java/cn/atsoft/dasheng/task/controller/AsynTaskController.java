a58adbb7918f7ebe91d5e41540519003f1cdf217
song <2625598959@qq.com>
2022-04-28 08:34:16 +0800

    库存导入
    Signed-off-by: song <2625598959@qq.com>

--- app/src/main/java/cn/atsoft/dasheng/task/controller/AsynTaskController.java
+++ app/src/main/java/cn/atsoft/dasheng/task/controller/AsynTaskController.java
@@ -60,6 +60,7 @@ public class AsynTaskController extends BaseController {
                 break;
             case "物料导入":
             case "产品导入":
+            case "库存导入":
                 Map<String, Integer> num = detailService.getNum(result.getTaskId());
                 result.setSuccessNum(num.get("successNum"));
                 result.setErrorNum(num.get("errorNum"));
