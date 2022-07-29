c1b177f4dd872fd6e5d6f15071fec8f6cc28d041
song <2625598959@qq.com>
2022-07-29 13:58:20 +0800

    入库处理结果 查看供应商
    Signed-off-by: song <2625598959@qq.com>

--- app/src/main/java/cn/atsoft/dasheng/erp/controller/InstockHandleController.java
+++ app/src/main/java/cn/atsoft/dasheng/erp/controller/InstockHandleController.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -101,42 +102,43 @@ public class InstockHandleController extends BaseController {
     @RequestMapping(value = "/list", method = RequestMethod.POST)
     @ApiOperation("列表")
     public PageInfo<InstockHandleResult> list(@RequestBody(required = false) InstockHandleParam instockHandleParam) {
-        if(ToolUtil.isEmpty(instockHandleParam)){
+        if (ToolUtil.isEmpty(instockHandleParam)) {
             instockHandleParam = new InstockHandleParam();
         }
         return this.instockHandleService.findPageBySpec(instockHandleParam);
     }
+
     /**
-         * 查询列表
-         *
-         * @author song
-         * @Date 2022-07-08
-         */
-        @RequestMapping(value = "/listByInstockOrderId", method = RequestMethod.GET)
-        @ApiOperation("列表")
-        public ResponseData listByInstockOrderId(@RequestParam Long instockOrderId) {
-            List<InstockHandle> instockHandles = this.instockHandleService.query().eq("instock_order_id", instockOrderId).eq("display", 1).list();
-            List<InstockHandleResult> instockHandleResults = BeanUtil.copyToList(instockHandles, InstockHandleResult.class);
+     * 查询列表
+     *
+     * @author song
+     * @Date 2022-07-08
+     */
+    @RequestMapping(value = "/listByInstockOrderId", method = RequestMethod.GET)
+    @ApiOperation("列表")
+    public ResponseData listByInstockOrderId(@RequestParam Long instockOrderId) {
+        List<InstockHandle> instockHandles = this.instockHandleService.query().eq("instock_order_id", instockOrderId).eq("display", 1).list();
+        List<InstockHandleResult> instockHandleResults = BeanUtil.copyToList(instockHandles, InstockHandleResult.class);
 
-            List<InstockHandleResult> detailTotalList = new ArrayList<>();
+        List<InstockHandleResult> detailTotalList = new ArrayList<>();
 
-            instockHandleResults.parallelStream().collect(Collectors.groupingBy(item -> item.getSkuId() + "_" + item.getBrandId()+"_"+item.getType(), Collectors.toList())).forEach(
-                    (id, transfer) -> {
-                        transfer.stream().reduce((a, b) -> new InstockHandleResult() {{
-                            setNumber(a.getNumber() + b.getNumber());
-                            setSkuId(a.getSkuId());
-                            setBrandId(a.getBrandId());
-                            setType(a.getType());
-                        }}).ifPresent(detailTotalList::add);
-                    }
-            );
+        instockHandleResults.parallelStream().collect(Collectors.groupingBy(item -> item.getSkuId() + "_" + item.getBrandId() + "_" + item.getType() + "_" + item.getCustomerId(), Collectors.toList())).forEach(
+                (id, transfer) -> {
+                    transfer.stream().reduce((a, b) -> new InstockHandleResult() {{
+                        setNumber(a.getNumber() + b.getNumber());
+                        setSkuId(a.getSkuId());
+                        setBrandId(a.getBrandId());
+                        setType(a.getType());
+                        setCustomerId(a.getCustomerId());
+                    }}).ifPresent(detailTotalList::add);
+                }
+        );
 
 
+        this.instockHandleService.format(detailTotalList);
 
-            this.instockHandleService.format(detailTotalList);
-
-            return ResponseData.success(detailTotalList) ;
-        }
+        return ResponseData.success(detailTotalList);
+    }
 
 }
 
