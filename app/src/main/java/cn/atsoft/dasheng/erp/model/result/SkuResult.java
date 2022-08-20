6ad82890b9558c20a4cf7d6879a6d6d39abe0171
Captain_Jazz <1045447343@qq.com>
2022-08-20 11:38:03 +0800

    字段隐藏修复

--- app/src/main/java/cn/atsoft/dasheng/erp/model/result/SkuResult.java
+++ app/src/main/java/cn/atsoft/dasheng/erp/model/result/SkuResult.java
@@ -64,7 +64,6 @@ public class SkuResult implements Serializable {
     private Long lackNumber;  //缺料数
     private Long storehouseId;
     private Long positionId;
-    @FieldPermission(value = true)
     private Integer stockNumber = 0;
     private List<StorehousePositionsResult> positionsResult;
     private Integer lockStockDetailNumber;
