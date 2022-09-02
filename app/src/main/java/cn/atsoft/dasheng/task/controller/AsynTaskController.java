07b8b5956e1d75599bc74b7f0f736ee63d4ad27e
song <2625598959@qq.com>
2022-09-02 08:55:15 +0800

    修改数量
    Signed-off-by: song <2625598959@qq.com>

--- app/src/main/java/cn/atsoft/dasheng/task/controller/AsynTaskController.java
+++ app/src/main/java/cn/atsoft/dasheng/task/controller/AsynTaskController.java
@@ -59,7 +59,6 @@ public class AsynTaskController extends BaseController {
                 result.setAllBomResult(allBomResult);
                 break;
             case "物料导入":
-            case "产品导入":
             case "库存导入":
             case "库位导入":
                 Map<String, Integer> num = detailService.getNum(result.getTaskId());
