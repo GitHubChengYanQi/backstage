a8dec43c2a513951486d237e7dd3349a3b6faf76
Captain_Jazz <1045447343@qq.com>
2022-12-26 09:48:29 +0800

    修复库位更新重名判断

--- app/src/main/java/cn/atsoft/dasheng/erp/service/impl/StorehousePositionsServiceImpl.java
+++ app/src/main/java/cn/atsoft/dasheng/erp/service/impl/StorehousePositionsServiceImpl.java
@@ -289,9 +289,9 @@ public class StorehousePositionsServiceImpl extends ServiceImpl<StorehousePositi
         }
 
         if (!oldEntity.getName().equals(newEntity.getName())) {
-            Integer count = this.query().eq("name", newEntity.getName()).eq("pid", newEntity.getPid()).count();
+            Integer count = this.query().eq("name", newEntity.getName()).eq("pid", newEntity.getPid()).ne("storehouse_positions_id",newEntity.getStorehousePositionsId()).count();
             if (count > 0) {
-                throw new ServiceException(500, "名字以重复");
+                throw new ServiceException(500, "名字已重复");
             }
         }
 
